fca381f41563e8ccb713b9be1ad448e96c12dc8f
OnurcanCekem <48911241+OnurcanCekem@users.noreply.github.com>
2021-12-21 10:39:12 +0100

    Added comment lines
    Added HALsensor and slider. Slider doesn't work because Raspberry Pi doesn't have analog inputs, will work with MCP3008 ADC.
    For the rest added comment lines.

--- Desktop/Testingbutton.py
+++ Desktop/Testingbutton.py
@@ -1,3 +1,4 @@
+# Button program to interrupt an LED from blinking
 # button pin 19, led 26
 # Connect LED to pin and GND
 # Connect button top left with resistor to GND
@@ -6,26 +7,29 @@
 import RPi.GPIO as GPIO
 import time
 
+#Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 sleepTime = 0.1
 
-lightPin = 26 #
+lightPin = 26 # LED
 buttonPin = 19 # buttonpin
 GPIO.setup(lightPin, GPIO.OUT)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Pull-up button
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Pull-down button
 
 try: # when program starts, go to this loop
     while True:
 
-        if GPIO.input(buttonPin) == GPIO.LOW:
+        if GPIO.input(buttonPin) == GPIO.LOW: # if low
             print("Low can I get witcha")
-        elif GPIO.input(buttonPin) == GPIO.HIGH:
+        elif GPIO.input(buttonPin) == GPIO.HIGH: # else high, but elif does the same
             print("High call back I'm busy")
-        GPIO.output(lightPin, GPIO.input(buttonPin))
+        GPIO.output(lightPin, GPIO.input(buttonPin)) # Turn lightpin on/off depending on the input of button
         time.sleep(0.1)
 
-finally: #when interrupted, do this
+#when keyinterrupted or at the end of program, do this
+# This is to prevent the LED from staying on when interrupted at the wrong time
+finally: 
     GPIO.output(lightPin, False)
     GPIO.cleanup()
                     
